0ee9d5dceb5a70e7e1cd937ec2b3c78af24bf5d7
Domenico Rescigno <domenico.rescigno@gmail.com>
2023-02-26 18:58:11 +0100

    Added gstreamer

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,9 @@
     plex-media-player
     gimp
     nextcloud-client
+    vlc
+    appimage-run
+    bottles
     gnome.gedit
     gnome.gnome-tweaks
     gnomeExtensions.clipboard-indicator
@@ -128,6 +131,14 @@
     vscode
     godot_4
     aseprite-unfree
+    # gstreamer
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base 
+    gst_all_1.gst-plugins-good 
+    gst_all_1.gst-plugins-bad 
+    gst_all_1.gst-plugins-ugly 
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
   ];
   
   environment.shells = with pkgs; [ 
