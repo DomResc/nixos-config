236a7c8412733e9fda439c80fff1fef69de30674
Domenico Rescigno <domenico.rescigno@gmail.com>
2023-03-02 18:31:08 +0100

    Switch to plasma

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,11 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      <nixos-hardware/common/pc>
+      <nixos-hardware/common/pc/ssd>
+      <nixos-hardware/common/cpu/amd>
+      <nixos-hardware/common/gpu/amd>
       ./hardware-configuration.nix
     ];
 
@@ -15,17 +19,8 @@
     kernelPackages = pkgs.linuxPackages_zen;
   };
 
-  # Hardware
-  hardware.cpu.amd.updateMicrocode = true;
-
-  networking.hostName = "domresc-desktop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
+  networking.hostName = "domresc-desktop"; 
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -49,9 +44,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -74,18 +69,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define user account.
   users.users.domresc = {
     isNormalUser = true;
     description = "Domenico Rescigno";
@@ -102,7 +88,7 @@
     "python-2.7.18.6"
   ];
 
-  # List packages installed in system profile. To search, run:
+  # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
     # core
     vim
@@ -111,15 +97,8 @@
     p7zip
     onlyoffice-bin
     plex-media-player
-    gimp
     nextcloud-client
     vlc
-    appimage-run
-    bottles
-    gnome.gedit
-    gnome.gnome-tweaks
-    gnomeExtensions.clipboard-indicator
-    gnomeExtensions.tray-icons-reloaded
     # programming
     obsidian
     vscode
@@ -143,32 +122,8 @@
     fish
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  # Initial installed version
+  system.stateVersion = "23.05";
 
   # Swap
   swapDevices = [{
@@ -192,20 +147,13 @@
   programs.fish = {
     enable = true;
     shellAbbrs = {
-      config = "sudo gedit /etc/nixos/configuration.nix";
-      updateConfig = "sudo git -C /etc/nixos pull";
-      upgrade = "sudo nixos-rebuild switch --upgrade-all";
+      config = "code ~/.dotfile/nixos-config";
+      upgrade = "sudo nixos-rebuild switch -I nixos-config=~/.dotfile/nixos-config --upgrade-all";
       clean = "sudo nix-collect-garbage --delete-older-than 30d";
-      resetGrid = "gsettings reset org.gnome.shell app-picker-layout";
     };
   };
 
-  programs.corectrl = {
-    enable = true;
-  };
-
-  # Services
-  services.fstrim = {
+    programs.corectrl = {
     enable = true;
   };
 }
